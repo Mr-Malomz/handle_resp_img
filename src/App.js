1677855e9e9c62dad8e01c519d95c07abb09f4f5
demola malomo <malomo.alaba@yahoo.com>
2022-04-11 22:05:12 +0100

    add cloudinary-react component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Image, Transformation, CloudinaryContext } from 'cloudinary-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        <h1>Responsive Image</h1>
       </header>
+      <div className='image-wrapper'>
+        <CloudinaryContext
+          cloud_name={process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}
+        >
+          <Image publicId='photo-1648737154547-b0dfd281c51e'>
+            <Transformation width='400' crop='scale' />
+          </Image>
+        </CloudinaryContext>
+      </div>
     </div>
   );
 }
